96d56a8241be9d672115d016be89c6cab360fd7d
pranavcpawar <pranavcpawar@outlook.com>
2024-03-21 16:36:03 +0530

    remove theme-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/ThemeProvider";
 
 const poppins = Poppins({weight: ["100", "200", "400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
 
@@ -18,9 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.className} dark flex flex-col min-h-screen items-center justify-between p-4`}>
-        <ThemeProvider>
-          {children}
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   );
