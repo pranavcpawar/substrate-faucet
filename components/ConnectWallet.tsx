362bd39e78e2b06d2017931a40c64bb9076b9558
pranavcpawar <pranavcpawar@outlook.com>
2024-03-22 21:04:40 +0530

    add connect wallet

---
+++ components/ConnectWallet.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConnectWallet = (): React.JSX.Element => {
+  return (
+    <button className="flex items-center justify-center px-4 py-2 bg-[#311C31] text-[#FC72FF] sm:text-lg font-medium rounded-[10px] active:scale-95">Connect Wallet</button>
+  );
+};
+
+export default ConnectWallet;
